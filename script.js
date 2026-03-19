a290f9708997ef82c0b2bc9270e862ed574640e0
SAMUEL VICTOR ASBAHR MACIEL <samuel.maciel5@senaisp.edu.br>
2026-03-19 09:44:57 -0300

    meu sexto commit

--- script.js
+++ script.js
@@ -3,42 +3,48 @@ const botao = document.getElementById("btnAdicionar");
 const lista = document.getElementById("listaTarefas");
 const mensagem = document.getElementById("mensagem");
 
-
 botao.addEventListener("click", function () {
 
     const texto = input.value;
 
-    
     if (texto === "") {
         mensagem.textContent = "Tarefa vazia!";
         mensagem.className = "text-danger fw-bold";
         return;
     }
-
+  
     const li = document.createElement("li");
     li.classList.add("list-group-item");
 
-    
-    li.textContent = texto;
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
 
+    
+    const span = document.createElement("span");
+    span.textContent = texto;
+
+  
     const botaoRemover = document.createElement("button");
     botaoRemover.textContent = "Remover";
     botaoRemover.classList.add("btn", "btn-danger", "btn-sm", "ms-2");
 
-    
-    li.appendChild(botaoRemover);
 
-   
+    checkbox.onchange = function () {
+    span.style.textDecoration = checkbox.checked ? "line-through" : "none";
+};
+
     botaoRemover.addEventListener("click", function () {
         li.remove();
     });
 
-  
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(botaoRemover);
+
     lista.appendChild(li);
 
-  
     input.value = "";
-  
+
     mensagem.textContent = "Tarefa adicionada!";
     mensagem.className = "text-success fw-bold";
 });
