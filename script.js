005d85d063d2d15572bd84add525a85c060d86d2
SAMUEL VICTOR ASBAHR MACIEL <samuel.maciel5@senaisp.edu.br>
2026-03-19 08:29:52 -0300

    meu primeiro commit na at2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-ajshfvmasf
+
+const botao = document.getElementById("btnAdicionar");
+const mensagem = document.getElementById("mensagem");
+
+
+mensagem.textContent = "Clique no botão para mudar este texto.";
+
+
+botao.addEventListener("click", function() {
+mensagem.textContent = "Texto altreado com JavaScript!";
+
+});
