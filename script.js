d16676ce684c9feacf345105f556cd30943fefd7
SAMUEL VICTOR ASBAHR MACIEL <samuel.maciel5@senaisp.edu.br>
2026-03-19 09:11:26 -0300

    meu quarto commit

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
+const input = document.getElementById("inputTarefa");
 const botao = document.getElementById("btnAdicionar");
+const lista = document.getElementById("listaTarefas");
 const mensagem = document.getElementById("mensagem");
 
-
-mensagem.textContent = "Clique no botão para mudar este texto.";
-
-
 botao.addEventListener("click", function() {
-mensagem.textContent = "Texto altreado com JavaScript!";
+
+    const texto = input.value;
+
+    
+    if (texto === "") {
+        mensagem.textContent = "Tarefa vazia!";
+        mensagem.className = "text-danger fw-bold";
+    } else {
+
+        
+        const novoItem = document.createElement("li");
+        novoItem.textContent = texto;
+        novoItem.classList.add("list-group-item");
+
+        lista.appendChild(novoItem);
+
+        input.value = "";
+
+      
+        mensagem.textContent = "tarefa adicionada com suceso!";
+        mensagem.className = "text-success fw-bold";
+    }
 
 });
